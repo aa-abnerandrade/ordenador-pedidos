e28c493a25363ea9bd61fefe0e03ff6f0a3af074
Abner Andrade <aa.abnerandrade@gmail.com>
2023-04-02 18:15:32 -0300

    Primeiro Commit

---
+++ script/cliente.js
@@ -0,0 +1,8 @@
+class Cliente {
+    constructor(inputNome, inputCPF) {
+        this.nome = inputNome;
+        this.cpf = inputCPF;
+        this.pedidos = [];
+    }
+
+}
---
+++ script/pedidos.js
@@ -0,0 +1,20 @@
+class Pedidos {
+    constructor(inputNumero, inputData, inputProduto, inputValor) {
+      this.numero = inputNumero;
+      this.data = inputData;
+      this.produto = inputProduto;
+      this.valor = inputValor;
+    }
+  }
+
+listaPedidos = [
+
+    { numeropedido: 1, data: 20/05/2019 },
+    { numeropedido: 7, data: 05/03/2023 },
+    { numeropedido: 5, data: 12/12/2022 },
+    { numeropedido: 3, data: 06/07/2021 },
+    { numeropedido: 2, data: 09/08/2020 },
+    { numeropedido: 4, data: 13/12/2021 },
+    { numeropedido: 0, data: 01/02/2023 },
+
+];
