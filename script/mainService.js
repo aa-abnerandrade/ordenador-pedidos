a0dbf6d893d4592f9d65240514b22d1f1864c4b2
Abner Andrade <aa.abnerandrade@gmail.com>
2023-04-03 16:27:01 -0300

    Alteração na interação entre classes Cliente e Pedido

--- script/mainService.js
+++ script/mainService.js
@@ -5,25 +5,22 @@ import { exibirClienteEmHTML , exibirPedidosEmHTML } from './contentHTML.js'
 
 export function obterCliente() {
     const cliente001 = new Cliente('João Silva', 'joao@email.com', '123.456.789-00');
-    cliente001.adicionarPedido();
-    cliente001.adicionarPedido();
     return 'https://raw.githubusercontent.com/aa-abnerandrade/zum_ordenaprecos/main/data/cliente.json';
 }
 
 
 export function carregarCliente(urlCliente) {
-
     (async ()=> {
         const jDados = await getFileData(urlCliente);
         var objCliente = construirCliente(jDados.dados);
-        var [lstPedidos] = construirPedidos(objCliente, jDados.pedidos);
-
+        var lstPedidos = construirPedidos(objCliente, jDados.pedidos);
     })();
 }
 
 function construirCliente(dadosCliente) {
     const cliente = new Cliente(dadosCliente.nome, dadosCliente.email, dadosCliente.cpf);
     exibirClienteEmHTML(cliente);
+    console.log(cliente)
     return cliente;
 }
 
@@ -33,7 +30,7 @@ function construirPedidos(objCliente, dadosPedidos) {
 
     dadosPedidos.forEach((pedido) => {
         console.log(pedido);
-        objCliente.adicionarPedido(pedido);
+        //objCliente.adicionarPedido(pedido);
         //exibirPedidosEmHTML(pedido);
     });
 
